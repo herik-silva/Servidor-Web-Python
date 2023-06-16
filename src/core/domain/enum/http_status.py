1f6077cdb41402f41571dcee3da2a23450952f3d
herik-silva <herik.silva.019@gmail.com>
2023-06-16 17:21:47 -0300

    Atualizado o méetodo handle_request, adicionando o status da resposta

--- src/core/domain/enum/http_status.py
+++ src/core/domain/enum/http_status.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 class HTTP_STATUS(Enum):
     OK = 200
-    ACCEPTED = 202
     NOT_FOUND = 404
+    INTERNAL_ERROR = 500
--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ import socket
 from core.domain.entities.server_address import ServerAddress
 from core.helpers.filter_element import filter_element
 from routes.public import *
+from core.domain.enum.http_status import HTTP_STATUS
 
 import os
 
@@ -19,13 +20,22 @@ class Server:
             ABOUT
         ]
 
-    def handle_request(self, content: bytes):
+    def set_status(self, status_code: int):
+        http_status = {
+            HTTP_STATUS.OK.value: "200 OK",
+            HTTP_STATUS.NOT_FOUND.value: "404 NOT FOUND",
+            HTTP_STATUS.INTERNAL_ERROR.value: "500 INTERNAL SERVER ERROR"
+        }
+
+        return http_status[status_code]
+
+    def handle_request(self, content: bytes, status_code):
         # Conteúdo da resposta
         response_content = content
 
         # Cabeçalhos da resposta
         response_headers = [
-            b"HTTP/1.1 200 OK",
+            b"HTTP/1.1 " + self.set_status(status_code).encode(),
             b"Content-Type: text/html",
             b"Content-Length: " + str(len(response_content)).encode(),
             b"Connection: close",
@@ -42,26 +52,27 @@ class Server:
     
     def send_response(self, data: bytes, client: socket.socket):
         route = self.get_route(data)
+
         print("Rota Solicitada: " + route)
-        founded_route = filter_element(self.__route_list, "route_name", route)
-        if(founded_route):
+        
+        route_found = filter_element(self.__route_list, "route_name", route)
+        if(route_found):
             try:
-                with open(os.getcwd() + founded_route.file_path, "rb") as file:
+                with open(os.getcwd() + route_found.file_path, "rb") as file:
                     content = file.read()
-                    response = self.handle_request(content)
+                    response = self.handle_request(content, HTTP_STATUS.OK.value)
                     client.send(response)
 
             except:
-                print("ARQUIVO NÃO ENCONTRADO")
-                with open(os.getcwd() + "/public/404.html", "rb") as file:
-                    content = file.read()
-                    response = self.handle_request(content)
-                    client.send(response)
+                print("PROBLEMA COM O ARQUIVO")
+                content = "Houve um problema ao processar a solicitação".encode()
+                response = self.handle_request(content, HTTP_STATUS.NOT_FOUND.value)
+                client.send(response)
         else:
             print("ROTA NAO EXISTE")
             with open(os.getcwd() + "/public/404.html", "rb") as file:
                 content = file.read()
-                response = self.handle_request(content)
+                response = self.handle_request(content, HTTP_STATUS.NOT_FOUND.value)
                 client.send(response)
 
         client.close()
